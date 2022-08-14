a82916f265127ae1e0d9ff74f5763cf70a59deb7
hectarea <gonzalescobar@gmail.com>
2022-08-14 01:06:47 -0600

    'built'

--- src/App.js
+++ src/App.js
@@ -163,7 +163,8 @@ React.useEffect(() => {
       
       <div id="Hs" className='snap-start pb-[17vh] overflow-y-scroll h-screen w-screen flex flex-col justify-start'>
       <p className='select-all mt-[17vh] font-["Cherry_Swash"] w-[100vh] max-w-[90vw] self-center text-justify text-[4vmin] md:text-[3.3vmin]'>AUE (Animal Uprising Event) is the name for upcoming incidents where animals will try to overcome humans for various reasons, including: destroying their natural habitats, deforestation, environmental pollution and abuse.
-      <p>Probability Form</p></p>
+     </p>
+     <button className='p-[1vh] bg-[#FF1053] text-slate-100 mt-[5vh] rounded-[2vmin] ml-[5vmin] text-[3vmin] md:self-center w-[37vmin]'>Probability Formula &rarr;</button>
       <p className='select-all mb-[1vh] w-[100vh] font-["Cherry_Swash"] mt-[17vh] max-w-[90vw] self-center text-[4vmin] text-center'>Do not panic, it is inmninent, preapare for the <a className='font-[Cinzel] font-thin text-[#FF1053]'>The  <a className='font-black'>A</a>nimal  <a className='font-black'>U</a>prising  <a className='font-black'>E</a>vent</a> instead.</p>
       <div className='flex flex-col'>
     <RareFishesClass/>
@@ -205,4 +206,5 @@ The sha256 hash of a UTF-8 string containing the current date in the format:
 Plus a UTF-8 string of any current threat asociated with animals, such as 'COVID-19' or 'Monkey Pox'
 </p>
       </div>
-  
+    )
+  }
